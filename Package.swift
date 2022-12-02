e14f527d9e8e0dcacea51a356edc2bf304322137
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2022-12-01 21:07:02 -0500

    Update swift-argument-parser version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-argument-parser",
-            from: "0.3.0"),
+            from: "1.0.0"),
     ],
     targets: [
         .target(
